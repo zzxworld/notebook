8db092dcca75053a1d8f1f263de458924afccb9b
zzxworld <zzxworld@gmail.com>
2017-08-21 21:49:06 +0800

    fixbug: PDO 参数引用提示

--- classes/user.php
+++ classes/user.php
@@ -5,29 +5,30 @@ class UserInvalidPasswordError extends Exception {};
 
 class User
 {
-	public static function encryptPassword($text, $salt)
-	{
-		return md5($text.':'.$salt);
-	}
+    public static function encryptPassword($text, $salt)
+    {
+        return md5($text.':'.$salt);
+    }
 
-	public static function create(array $params)
-	{
+    public static function create(array $params)
+    {
         $total = DB::count('users', ['email' => $params['email']]);
-		if ($total > 0) {
-			throw new UserRepeatAccountError();
-		}
+        if ($total > 0) {
+            throw new UserRepeatAccountError();
+        }
 
         $salt = randString(8);
-		$password = self::encryptPassword($params['password'], $salt);
+        $password = self::encryptPassword($params['password'], $salt);
+        $date = date('Y-m-d H:i:s');
 
-		$query = DB::prepare('INSERT INTO users(email, password, salt, created_at)
-			VALUES(:email, :password, :salt, :created_at)');
-		$query->bindParam(':email', $params['email']);
-		$query->bindParam(':password', $password);
-		$query->bindParam(':salt', $salt);
-		$query->bindParam(':created_at', date('Y-m-d H:i:s'));
-		$query->execute();
-	}
+        $query = DB::prepare('INSERT INTO users(email, password, salt, created_at)
+            VALUES(:email, :password, :salt, :created_at)');
+        $query->bindParam(':email', $params['email']);
+        $query->bindParam(':password', $password);
+        $query->bindParam(':salt', $salt);
+        $query->bindParam(':created_at', $date);
+        $query->execute();
+    }
 
     public static function login($account, $password)
     {
